4f049583c228557f8934d97f9d8526d8a19025b0
Joss Moffatt <josshmoffatt@gmail.com>
2022-08-03 17:44:01 +0100

    Adding java validation harness

--- polywit/validation_harnesses/resources/java/Test.java
+++ polywit/validation_harnesses/resources/java/Test.java
@@ -2,7 +2,7 @@ public class Test {
   public static void main(String[] args) {
     try {
       Main.main(new String[0]);
-      System.out.println("wit4java: Witness Spurious");
+      System.out.println("polywit: Witness Spurious");
     } catch (Exception e) {
       System.out.println(e);
       e.printStackTrace();
