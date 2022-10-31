16e934773cb8cc290e7cc7b48f8f0428e32f0fc5
Joss Moffatt <josshmoffatt@gmail.com>
2022-10-31 20:49:51 +0000

    :label: Adding type aliases to improve readability

---
+++ polywit/types/__init__.py
@@ -0,0 +1,5 @@
+from polywit.types.aliases import AssumptionList
+
+__all__ = [
+    AssumptionList
+]
---
+++ polywit/types/aliases.py
@@ -0,0 +1,4 @@
+from typing import TypeAlias, List, Tuple
+
+Assumption: TypeAlias = Tuple[str, str]
+AssumptionList: TypeAlias = List[Assumption, str]
