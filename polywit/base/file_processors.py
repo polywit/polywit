d49a12122a859bce2431f51cd63879cfab1a6fdf
Joss Moffatt <josshmoffatt@gmail.com>
2022-11-10 17:26:48 +0000

    :memo: Removed unused import

--- polywit/base/file_processors.py
+++ polywit/base/file_processors.py
@@ -9,7 +9,7 @@ from abc import ABC, abstractmethod
 import os
 import networkx as nx
 
-from polywit.types.aliases import Assumption, Position
+from polywit.types.aliases import Position
 
 
 class Processor(ABC):
