122dc2e4de61e7f2cc531a343fbe138b4194c534
Joss Moffatt <josshmoffatt@gmail.com>
2022-12-23 18:12:04 +0000

    :rotating_light: Linting

--- polywit/exceptions/exceptions.py
+++ polywit/exceptions/exceptions.py
@@ -1,11 +1,14 @@
 class ValidationException(Exception):
     pass
 
+
 class WitnessProcessorException(ValidationException):
     pass
 
+
 class FileProcessorException(ValidationException):
     pass
+
 
 class TestHarnessException(ValidationException):
     pass
