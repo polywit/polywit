c6ef9634d64cb693ab379822bb07269d39fe0d58
Joss Moffatt <josshmoffatt@gmail.com>
2023-03-18 21:04:43 +0000

    :sparkles: Adding exceptions for different phases

--- polywit/exceptions/exceptions.py
+++ polywit/exceptions/exceptions.py
@@ -1,18 +1,62 @@
 class ValidationError(Exception):
-    pass
+    def __init__(self, phase):
+        self.message = f'Polywit has encountered an issue during {phase}.'
+        self.retry_message = 'Retry using --stacktrace to see the full stacktrace.'
+        super().__init__(self.message)
 
 
 class WitnessProcessorError(ValidationError):
-    pass
+    def __init__(self, phase="witness processing"):
+        super().__init__(phase)
 
 
-class WitnessFormatError(WitnessProcessorError):
-    pass
+class WitnessPreprocessingError(WitnessProcessorError):
+    PHASE = 'witness preprocessing'
+
+    def __init__(self):
+        super().__init__(WitnessPreprocessingError.PHASE)
+
+
+class AssumptionExtractionError(WitnessProcessorError):
+    PHASE = 'assumption extraction'
+
+    def __init__(self):
+        super().__init__(AssumptionExtractionError.PHASE)
 
 
 class FileProcessorError(ValidationError):
-    pass
+    def __init__(self, phase="file processing"):
+        super().__init__(phase)
+
+
+class FilePreprocessingError(FileProcessorError):
+    PHASE = 'file preprocessing'
+
+    def __init__(self):
+        super().__init__(FilePreprocessingError.PHASE)
+
+
+class PositionTypeExtractionError(WitnessProcessorError):
+    PHASE = 'position type extraction'
+
+    def __init__(self):
+        super().__init__(AssumptionExtractionError.PHASE)
 
 
 class TestHarnessError(ValidationError):
-    pass
+    def __init__(self, phase='test harness development'):
+        super().__init__(phase)
+
+
+class TestHarnessExecutionError(TestHarnessError):
+    PHASE = 'execution of test harness'
+
+    def __init__(self):
+        super().__init__(TestHarnessExecutionError.PHASE)
+
+
+class TestHarnessConstructionError(TestHarnessError):
+    PHASE = 'construction of test harness'
+
+    def __init__(self):
+        super().__init__(TestHarnessConstructionError.PHASE)
